04ea9641c2c18077b67623ba6fe37405ba4199b4
C G <catalin_gafton@yahoo.com>
2018-12-20 02:48:01 +0200

    https://github.com/CatalinGafton/homework2.git

--- homework/Program.cs
+++ homework/Program.cs
@@ -2,14 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace homework
+namespace IfExample
 {
-    class Program
+    class DateTimeExtension
     {
-        static void Main(string[] args)
+        public string ToAgeString(DateTime dob)
         {
+            DateTime dt = DateTime.Now;
+
+            int days = dt.Day - dob.Day;
+            if (days < 0)
+            {
+                dt = dt.AddMonths(-1);
+                days += DateTime.DaysInMonth(dt.Year, dt.Month);
+            }
+
+            int months = dt.Month - dob.Month;
+            if (months < 0)
+            {
+                dt = dt.AddYears(-1);
+                months += 12;
+            }
+
+            int years = dt.Year - dob.Year;
+
+            return string.Format("{0} year{1}, {2} month{3} and {4} day{5}",
+            years, (years == 1) ? "" : "s",
+            months, (months == 1) ? "" : "s",
+            days, (days == 1) ? "" : "s");
+        }
+
+        static void Main()
+        {
+            DateTimeExtension obj = new DateTimeExtension();
+            Console.WriteLine("Enter your date of birth in mm/dd/yyyy format to calculate age");
+            DateTime dob = DateTime.Parse(Console.ReadLine());
+            string s = obj.ToAgeString(dob);
+            Console.WriteLine(s);
+            Console.ReadLine();
         }
     }
 }
